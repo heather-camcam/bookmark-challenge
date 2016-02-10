55806069741ed824755a0b0785504c9c2c032c87
itsindigo <matthewbridgesbusiness@gmail.com>
2016-02-10 11:27:20 +0000

    ...

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,6 +10,9 @@ class Link
   property :added_at,   DateTime
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_development")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
+
+
+# https://mark-my-books.herokuapp.com/
