b44d02a7c352b65d96a82b9c84b7d3559efcd15f
Heather <campbell.hf@gmail.com>
2016-02-11 18:50:23 +0000

    updated db so that user model is now recognised

--- app/app.rb
+++ app/app.rb
@@ -40,20 +40,20 @@ set :session_secret, 'super secret'
   end
 
   post '/signup' do
-    User.create(email: params[:email],
+    user = User.create(email: params[:email],
                 password: params[:password])
     session[:user_id] = user.id
     redirect '/signup/confirmation'
+  end
+
+  get '/confirmation' do
+    erb :'confirmation'
   end
 
   helpers do
     def current_user
       @current_user ||= User.get(session[:user_id])
     end
-  end
-
-  get '/confirmation' do
-    erb :'confirmation'
   end
 
   # start the server if ruby file executed directly
--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -7,7 +7,7 @@ feature 'user login' do
     expect{create_account}.to change(User, :count).by(1)
   end
 
-  scenario 'redirects to welcome page about account creation' do
+  scenario 'redirects to welcome page after account creation' do
     create_account
     expect(page).to have_content 'Welcome test@test.com!'
   end
